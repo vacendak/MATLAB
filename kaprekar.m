c88c113c685236961166dcfb2844267c060ab140
SERGIO SANZ HERNÁNDEZ <49347859+vacendak@users.noreply.github.com>
2025-11-20 17:12:34 +0100

    Actualizar kaprekar.m
    Refactoring

--- kaprekar.m
+++ kaprekar.m
@@ -39,16 +39,13 @@ function difference = kap(number)
     % 2. CONSTRUCT THE STRING
     % Formats each digit ('%d') into a single string.
     string_1 = sprintf('%d', sorted_digits_1);
-
     string_2 = sprintf('%d', sorted_digits_2);
 
     % 3. Convert to number
     largest_number = str2double(string_1);
-
     smallest_number = str2double(string_2);
 
     disp(['Reordered number (largest): ', num2str(largest_number)])
-
     disp(['Reordered number (smallest): ', num2str(smallest_number)])
 
     % 4. Calculate the difference
