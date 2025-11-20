244ed284cb413f35bb5963d102e6553a732bf88f
SERGIO SANZ HERNÁNDEZ <49347859+vacendak@users.noreply.github.com>
2025-11-20 17:11:19 +0100

    Actualizar kaprekar.m
    Bug fixing to support less than 4 digits numbers.

--- kaprekar.m
+++ kaprekar.m
@@ -1,20 +1,30 @@
 % Original number
 % Four digits number with at least 2 different 
 
-% Choose a number to check
-original_number = 7648; 
-disp(['Original number: ', num2str(original_number)])
-difference = 0;
+original_number = input('Input a number (4 digits or less): ');
 
+disp(['Original number: ', num2str(original_number)])
+iter = 1;
 % First calculation (difference is returned)
 difference = kap(original_number); 
 
 for i = 1:10
     if difference ~= 6174
-    % Second calculation (difference is passed back in, new difference is returned)
+        iter = iter + 1; % Increment iteration count
+        % Second calculation (difference is passed back in,
+        %    new difference is returned)
+        if difference <= 999
+            l_diff = length(num2str(difference)); 
+            zeros2add = 4 - l_diff; 
+            for i = 1:zeros2add
+                difference = difference * 10
+            end
+        end
         difference = kap(difference); 
     end
 end
+disp(['# of iter.: ', num2str(iter)])
+
 
 function difference = kap(number)
     % 1. Get and sort the digits
@@ -29,13 +39,16 @@ function difference = kap(number)
     % 2. CONSTRUCT THE STRING
     % Formats each digit ('%d') into a single string.
     string_1 = sprintf('%d', sorted_digits_1);
+
     string_2 = sprintf('%d', sorted_digits_2);
 
     % 3. Convert to number
     largest_number = str2double(string_1);
+
     smallest_number = str2double(string_2);
 
     disp(['Reordered number (largest): ', num2str(largest_number)])
+
     disp(['Reordered number (smallest): ', num2str(smallest_number)])
 
     % 4. Calculate the difference
